a99a4f1bf73b048000d1bcefc71d49c63f370541
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-10-16 11:21:25 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,9 +14,9 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {railroads: {}}
-	monopoly[railroads][pieces] = 4
-	monopoly[railroads][names] = {}
-	monopoly[railroads][rent_in_dollars] = {}
+	monopoly[railroads][:pieces] = 4
+	monopoly[railroads][:names] = {}
+	monopoly[railroads][:rent_in_dollars] = {}
 
 end
 
